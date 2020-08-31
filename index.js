ba2b52013d7545a8db5376cfb04d3bc46d1e299f
Ovidio Pérez <60124007+ovirex@users.noreply.github.com>
2020-08-31 02:52:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+    let titleCont = document.getElementsByClassName("title-container")[0]
+        .children;
+
+    for (let i = 0; i < titleCont.length; i++) {
+        titleCont[i].addEventListener("mouseover", productIn);
+        titleCont[i].addEventListener("mouseout", productOut);
+    }
+
+    function productIn() {
+        let titleId = this.id;
+        let aceiteTipo = titleId;
+        let envaseImg = document.getElementById("envase-" + aceiteTipo);
+        console.log(aceiteTipo, envaseImg);
+
+        if (envaseImg.style.transitionTimingFunction == "ease-in") {
+            envaseImg.style.transitionTimingFunction = "ease-out";
+        }
+
+        switch (titleId) {
+            case "gasolina":
+                envaseImg.style.left = "280px";
+                break;
+            case "diesel":
+                envaseImg.style.left = "280px";
+                break;
+            case "borda":
+                envaseImg.style.left = "280px";
+                break;
+            case "motos":
+                envaseImg.style.left = "280px";
+                break;
+            case "transmisiones":
+                envaseImg.style.left = "280px";
+                break;
+            case "industriales":
+                envaseImg.style.left = "280px";
+                break;
+        }
+    }
+
+    function productOut() {
+        let titleId = this.id;
+        let aceiteTipo = titleId;
+        let envaseImg = document.getElementById("envase-" + aceiteTipo);
+        envaseImg.style.left = "-3000px";
+        envaseImg.style.transitionTimingFunction = "ease-in";
+        console.log(aceiteTipo);
+    }
+};
